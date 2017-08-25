5760b49e8db8de378155657bd502edd659841519
Hamza Hajji <hmzhjji@gmail.com>
2017-08-25 06:47:29 +0100

    create post /todos route

--- server/models/user.js
+++ server/models/user.js
@@ -9,4 +9,4 @@ var User = mongoose.model('User', {
   }
 });
 
-module.exports = {Todo};
+module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -1,4 +1,24 @@
-const {mongoose} = require('./db/mongoose');
+var app = require('express')();
+const bodyParser = require('body-parser');
 
-const {Todo} = require('.models/todo');
-const {User} = require('.models/user');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((todo) => {
+    res.send(todo);
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
