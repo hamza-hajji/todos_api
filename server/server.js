393dced24a17a8b75e9306f21a19b84072fa848f
Hamza Hajji <hmzhjji@gmail.com>
2017-08-25 17:46:23 +0100

    add get /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -7,6 +8,7 @@ const {User} = require('./models/user');
 
 app.use(bodyParser.json());
 
+// POST /todos
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
@@ -19,11 +21,34 @@ app.post('/todos', (req, res) => {
   });
 });
 
+// GET /todos
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos}); // {todos: [...], } to add more props later
   }, (err) => {
     res.status(400).send(err);
+  });
+});
+
+// GET /todos/123
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  // validate ID
+    // if not valid => 404, {}
+  // findById
+    // success
+      // if there is todo => send it
+      // if not => send 404, {}
+    // error
+      // 400 - send {}
+  if (!ObjectID.isValid(id)) return res.status(404).send({});
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) return res.status(404).send({});
+    res.send({todo});
+  }, (e) => {
+    console.log(e);
+    res.status(400).send({})
   });
 });
 
