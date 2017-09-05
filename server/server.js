9a61cb69a15d7bd916f78b6242a1c36cfae3a0e3
Hamza Hajji <hmzhjji@gmail.com>
2017-09-05 14:43:03 +0100

    add post /users/login route

--- server/server.js
+++ server/server.js
@@ -119,6 +119,19 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+// POST /users/login
+app.post('/users/login', (req, res) => {
+  var {email, password} = req.body;
+
+  User.findByCredentials(email, password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 if (!module.parent) {
   app.listen(port, () => {
